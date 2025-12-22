2df6f6b426b4609b537a6c74ddd4bc810b957205
Vlad Stanescu <vladstanescu94@gmail.com>
2025-12-22 13:30:19 +0200

    Initial Commit

---
+++ DiamerisTests/DiamerisTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiamerisTests.swift
+//  DiamerisTests
+//
+//  Created by Vlad Stanescu on 22.12.2025.
+//
+
+import Testing
+@testable import Diameris
+
+struct DiamerisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
